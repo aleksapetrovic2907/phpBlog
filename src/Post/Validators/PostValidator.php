37a58c315e1a201bc8165ef2894b210dd229881b
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-03-08 00:19:24 +0100

    Add title validation to PostValidator

--- src/Post/Validators/PostValidator.php
+++ src/Post/Validators/PostValidator.php
@@ -1,7 +1,33 @@
 <?php
 class PostValidator
 {
+    public const TITLE_MAXIMUM_LENGTH = 255;
     public const PROHIBITED_WORDS = ["CCP", "China Communist Party"];
+
+    /**
+     * Validates the title of a post.
+     *
+     * @param string $title The title to validate.
+     * @return array Returns an array of error messages if validation fails, or an empty array if valid.
+     */
+    public function validateTitle($title): array
+    {
+        $errors = [];
+
+        if (strlen($title) === 0)
+            $errors[] = "Title cannot be empty";
+
+        if (strlen($title) > self::TITLE_MAXIMUM_LENGTH)
+            $errors[] = "Title cannot be longer than " . self::TITLE_MAXIMUM_LENGTH . " characters";
+
+        foreach (self::PROHIBITED_WORDS as $word) {
+            if (stripos($title, $word) !== false) {
+                $errors[] = "Title contains prohibited word: '$word'";
+            }
+        }
+
+        return $errors;
+    }
 
     /**
      * Validates the content of a post.
