dff7b10797a158114b165025adaad158762b9400
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-03-07 16:29:59 +0100

    Create HashService class for handling password hashing

---
+++ src/Services/HashService.php
@@ -0,0 +1,19 @@
+<?php
+class HashService
+{
+    public function getHashAlgo(): string
+    {
+        return PASSWORD_BCRYPT;
+    }
+
+    public function hashPassword(string $password): string
+    {
+        return password_hash($password, self::getHashAlgo());
+    }
+
+    public function verifyPassword(string $password, string $passwordHash): bool
+    {
+        return password_verify($password, $passwordHash);
+    }
+}
+?>
