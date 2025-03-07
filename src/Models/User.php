32abc73680c0009a9bd52d8eedb0c0a21b1cb7cb
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-03-07 16:29:37 +0100

    Create User model

---
+++ src/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+namespace Src\Models;
+
+class User
+{
+    public int $id;
+    public string $username;
+    public string $passwordHash;
+    public string $createdAt;
+
+    public function __construct($id, $username, $passwordHash, $createdAt)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->passwordHash = $passwordHash;
+        $this->createdAt = $createdAt;
+    }
+}
+?>
