c6a33580046c4221bf33d7a0702a6af0d5c1ce4f
aleksapetrovic2907 <aleksa.petrovic2907@gmail.com>
2025-03-17 21:45:05 +0100

    Fix routes expecting parameters in the Uri rather than as query params

--- index.php
+++ index.php
@@ -6,27 +6,28 @@ $requestUri = str_replace($basePath, '', $_SERVER['REQUEST_URI']);
 $requestUri = trim($requestUri);
 $requestUri = parse_url($requestUri, PHP_URL_PATH);
 $requestMethod = $_SERVER['REQUEST_METHOD'];
+var_dump($requestUri);
 
 $routes = [
     'GET' => [
         'api/user' => 'src/User/Api/users.php',
         'api/post' => 'src/Post/Api/get_post.php',
-        'api/comment/{postId}' => 'src/Comment/Api/get_comments.php',
-        'api/like/{postId}/likers' => 'src/Like/Api/get_likers.php',
-        'api/like/{postId}/likesCount' => 'src/Like/Api/get_likes_count.php',
+        'api/comment' => 'src/Comment/Api/get_comments.php',
+        'api/like/likers' => 'src/Like/Api/get_likers.php',
+        'api/like/likesCount' => 'src/Like/Api/get_likes_count.php',
     ],
     'POST' => [
         'api/user' => 'src/User/Api/register.php',
         'api/auth/login' => 'src/Auth/Api/login.php',
         'api/auth/logout' => 'src/Auth/Api/logout.php',
         'api/post' => 'src/Post/Api/create_post.php',
-        'api/comment/{postId}' => 'src/Comment/Api/create_comment.php',
-        'api/like/{postId}' => 'src/Comment/Api/like_post.php',
+        'api/comment' => 'src/Comment/Api/create_comment.php',
+        'api/like' => 'src/Comment/Api/like_post.php',
     ],
     'DELETE' => [
         'api/post' => 'src/Post/Api/delete_post.php',
         'api/comment' => 'src/Comment/Api/delete_comment.php',
-        'api/like/{postId}' => 'src/Like/Api/unlike_post.php',
+        'api/like' => 'src/Like/Api/unlike_post.php',
     ],
     'PATCH' => [
         'api/post' => 'src/Post/Api/update_post.php',
@@ -35,8 +36,6 @@ $routes = [
 
 $matchedRoute = null;
 $params = [];
-
-var_dump($requestUri);
 
 foreach ($routes[$requestMethod] ?? [] as $route => $filePath) {
     $pattern = '#^' . preg_replace('/\{([a-zA-Z0-9_]+)\}/', '(?P<\1>[a-zA-Z0-9_]+)', $route) . '$#';
